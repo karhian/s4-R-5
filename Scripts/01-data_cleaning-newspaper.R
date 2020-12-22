1a8ed299ef1163dbec2f8a3e578afefee315720f
Kar Hian Ong <kar.ong@mail.utoronto.ca>
2020-12-23 02:16:15 +0800

    latest update on the paper

--- Scripts/01-data_cleaning-newspaper.R
+++ Scripts/01-data_cleaning-newspaper.R
@@ -17,7 +17,8 @@ newspapers <-
   newspapers %>% 
   dplyr::select(year, id_news, after_national, local, national, # Diff in diff variables
                 ra_cst, qtotal, ads_p4_cst, ads_s, # Advertising side dependents
-                ps_cst, po_cst, qtotal, qs_s, rs_cst) %>% #Reader side dependents
+                ps_cst, po_cst, qtotal, qs_s, rs_cst #Reader side dependents
+                ,nb_journ) %>% 
   mutate(ra_cst_div_qtotal = ra_cst / qtotal) %>% # An advertising side dependents needs to be built
   mutate_at(vars(id_news, after_national, local, national), ~as.factor(.)) %>% # Change some to factors
   mutate(year = as.integer(year))
